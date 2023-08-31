27c461c78eee24ba09d6dc519296d816033ce838
lilfall <m.falah.isnan@gmail.com>
2023-08-31 14:11:19 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const app = express();
+const port = 3070;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello guys!");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
