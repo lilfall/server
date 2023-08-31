aee118caec9b160c49b3d82dfe5c35604faf085d
lilfall <m.falah.isnan@gmail.com>
2023-08-31 16:14:47 +0700

    async

--- app/controllers/userControllers.js
+++ app/controllers/userControllers.js
@@ -1,7 +1,7 @@
 const prisma = require("../../prisma/client.js");
 
-exports.getUsers = (req, res) => {
-  prisma.user
+exports.getUsers = async (req, res) => {
+  await prisma.user
     .findMany()
     .then((users) => {
       res.status(200).json({ message: "berhasil", data: users });
