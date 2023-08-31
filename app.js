b147727bcd708b5273c15dc5817d107f7bcded66
lilfall <m.falah.isnan@gmail.com>
2023-08-31 15:30:06 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   app.js
    	modified:   app/routes/userRoutes.js
    	modified:   package-lock.json
    	modified:   package.json

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import userRoutes from "./app/routes/userRoutes.js";
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRouters = require("./app/routes/userRoutes.js");
 
 const app = express();
 const port = 3070;
@@ -10,11 +10,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use("/user", userRouters);
 app.get("/", (req, res) => {
   res.send("Hello guys berhasil diuptae!");
 });
-
-app.use("/user", userRoutes);
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
