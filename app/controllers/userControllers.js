f25011e364fd3842b7e04d07ca0367363807ba38
lilfall <m.falah.isnan@gmail.com>
2023-08-31 16:02:25 +0700

    fixing looping

--- app/controllers/userControllers.js
+++ app/controllers/userControllers.js
@@ -1,16 +1,12 @@
 const prisma = require("../../prisma/client.js");
 
 exports.getUsers = (req, res) => {
-  try {
-    prisma.user
-      .findMany()
-      .then((users) => {
-        res.status(200).json({ message: "berhasil", data: users });
-      })
-      .catch((error) => {
-        throw error;
-      });
-  } catch (error) {
-    throw error;
-  }
+  prisma.user
+    .findMany()
+    .then((users) => {
+      res.status(200).json({ message: "berhasil", data: users });
+    })
+    .catch((error) => {
+      throw error;
+    });
 };
